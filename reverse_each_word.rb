8c7bb244686ec6da859ecb9a8a63691b346c5670
Heath Mast <hrmast@gmail.com>
2020-04-12 17:52:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   str = string.split(" ")
   arr = []
-  arr = str.collect {|words| words.reverse}
+  arr = str.collect { |words| words.reverse }
   arr.join(" ")
 end
